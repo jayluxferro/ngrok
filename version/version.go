ea5afa0ca9e385e007948ffd4c0b38d2b4a980cf
Jay <jay@sperixlabs.org>
2025-12-13 01:16:29 +0000

    v1.0.0

--- version/version.go
+++ version/version.go
@@ -7,15 +7,20 @@ import (
 const (
 	Proto = "2"
 	Major = "1"
-	Minor = "7"
+	Minor = "0"
+	Patch = "0"
 )
 
 func MajorMinor() string {
 	return fmt.Sprintf("%s.%s", Major, Minor)
 }
 
+func MajorMinorPatch() string {
+	return fmt.Sprintf("%s.%s.%s", Major, Minor, Patch)
+}
+
 func Full() string {
-	return fmt.Sprintf("%s-%s.%s", Proto, Major, Minor)
+	return fmt.Sprintf("%s-%s.%s.%s", Proto, Major, Minor, Patch)
 }
 
 func Compat(client string, server string) bool {
